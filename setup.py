70b5ec9a0f83f63605bd25dfe079efb9194f507f
Yenson <yenson@evenup.ai>
2023-08-25 00:51:19 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from os import path
 
 from setuptools import setup
 
-dir_path = path.dirname(path.realpath(__file__))
+repo_path = path.dirname(path.realpath(__file__))
 
-with open("requirements.txt", "r") as f:
+with open(path.join(repo_path, "requirements.txt"), "r") as f:
     requirements = f.read().splitlines()
 
 setup(name="llm_playbooks", version="0.0.1", install_requires=requirements)
