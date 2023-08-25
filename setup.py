4840f7ed2538f35fcece9b4ce7d66eff1832c79a
Yenson <yenson@evenup.ai>
2023-08-25 00:22:59 -0400

    adding precommit hook and requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+from os import path
+
 from setuptools import setup
 
+dir_path = path.dirname(path.realpath(__file__))
 
-setup(
-    name="llm_playbooks",
-    version="0.0.1",
-    install_requires=[]
-)
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(name="llm_playbooks", version="0.0.1", install_requires=requirements)
