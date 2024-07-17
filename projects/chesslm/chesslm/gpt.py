69b14b6da05b49841307573719d2036906bae718
Yenson Lau <yenson.lau@gmail.com>
2024-07-17 02:57:05 +0000

    got gpt2 generating, but stuck in infinite loops

--- projects/chesslm/chesslm/gpt.py
+++ projects/chesslm/chesslm/gpt.py
@@ -1,5 +1,8 @@
 # Temporary home for a GPT implementation; expect to move this up to llm_playbooks later
+# Largely follows the tutorial from https://github.com/karpathy/build-nanogpt/
+
 import math
+from typing import Literal, get_args
 
 import torch
 import torch.nn as nn
@@ -18,6 +21,8 @@ from torch.nn import functional as F
 # n_layer = 6
 # dropout = 0.2
 
+PretrainedGPT2Model = Literal["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"]
+
 
 class GPTConfig(BaseModel):
     block_size: int = 256
@@ -25,15 +30,6 @@ class GPTConfig(BaseModel):
     n_layer: int = 6
     n_head: int = 6
     n_embd: int = 384
-
-
-GPT2_CONFIG = GPTConfig(
-    block_size=1024,  # max sequence lengths
-    vocab_size=50257,  # 50k BPE merges + 256 bytes tokens + 1 <|endoftext|>
-    n_layer=12,
-    n_head=12,
-    n_embd=768,
-)
 
 
 class CausalSelfAttention(nn.Module):
@@ -122,19 +118,86 @@ class GPT(nn.Module):
         )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    def forward(self, idx):
+    def forward(self, idx: torch.Tensor):
         B, T = idx.size()
-        assert T <= self.config.block_size, (
-            f"sequence length {T} should not exceed block size {self.config.block_size}"
-        )
+        assert (
+            T <= self.config.block_size
+        ), f"sequence length {T} should not exceed block size {self.config.block_size}"
 
-        pos = torch.arange(0, T, dtype = torch.long, device = idx.device)
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
         pos_emb = self.transformer.wpe(pos)  # (T, n_embd)
         tok_emb = self.transformer.wte(idx)  # (B, T, n_embd)
 
         x = tok_emb + pos_emb
         for block in self.transformer.h:
             x = block(x)
-        x = self.transformer. ln_f(x)
+        x = self.transformer.ln_f(x)
+
         logits = self.lm_head(x)  # (B, T, vocab_size)
         return logits
+
+    @classmethod
+    def from_pretrained(cls, model_type: PretrainedGPT2Model):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in get_args(PretrainedGPT2Model)
+
+        from transformers import GPT2LMHeadModel
+
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
+        }[model_type]
+
+        config_args["vocab_size"] = 50257  # always 50257 for GPT model checkpoints
+        config_args["block_size"] = 1024  # always 1024 for GPT model checkpoints
+
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [
+            k for k in sd_keys if not k.endswith(".attn.bias")
+        ]  # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [
+            k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")
+        ]  # ignore these, just a buffer
+        sd_keys_hf = [
+            k for k in sd_keys_hf if not k.endswith(".attn.bias")
+        ]  # same, just the mask (buffer)
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(
+            sd_keys
+        ), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
